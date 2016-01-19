6d06ddfffd0743ecee6ad8a0212eeaefe71e303c
Spyros Magripis <mechoulas@gmail.com>
2016-01-20 00:04:12 +0100

    Create classExample.php
    A simple php class example with properties and constructor.

---
+++ classExample.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	  <title>Reconstructing the Person Class</title>
+      <link type='text/css' rel='stylesheet' href='style.css'/>
+	</head>
+	<body>
+      <p>
+        <!-- Your code here -->
+        <?php
+        
+        class Person {
+            
+        public $isAlive = true;
+        public $firstname;
+        public $lastname;
+        public $age;
+            
+            
+        public function __construct($firstname, $lastname, $age) {
+        
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->age = $age;
+        
+        }
+        
+        }
+        
+        $teacher = new Person("boring","12345",12345);
+        $student = new Person("Spyros","12345",12345);   
+        
+        echo $teacher->isAlive;
+        echo $student->age;
+        
+        ?>
+      </p>
+    </body>
+</html>
